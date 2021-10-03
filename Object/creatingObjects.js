da596ba1f67f7079fee5607f03691f98d9bb01da
Marianne Bravo <mloubravo@gmail.com>
2021-10-03 17:20:41 -0300

    Create creatingObjects.js

---
+++ Object/creatingObjects.js
@@ -0,0 +1,12 @@
+const circle = { // {} means object
+	radius: 1, // proprieties
+	location: { // proprieties
+		x: 1,
+		y: 1,
+	},
+	draw: function() { // methods
+		console.log('draw');
+	}
+};
+
+circle.draw();
